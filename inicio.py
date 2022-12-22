b05c00c4950259e5bafa4c1b1a3b58364a2f7d51
Esley <esleynathan@hotmail.com>
2022-12-21 23:58:00 -0300

    CRUD PYTHON + MYSQL | Criando tabela

--- inicio.py
+++ inicio.py
@@ -6,6 +6,23 @@ con = pymysql.connect(
     user = "root",
     password = "",
     port = 3306,
+    db="aulapythonfull",
     charset = 'utf8mb4',
-    cursorclass = pymysql.cursors.DictCursor
-)
+    cursorclass = pymysql.cursors.DictCursor)
+
+def criaTabela(nomeTabela):
+    try:   
+        with con.cursor() as cursor:
+            cursor.execute("create table teste (nome varchar(50))")
+        print("Tabela criada com sucesso.")
+    except Exception as e:
+        print (f'Ocorreu um erro {e}')
+
+def removerTabela(nomeTabela):
+    try:   
+        with con.cursor() as cursor:
+            cursor.execute(f"drop table {nomeTabela}")
+        print("Tabela excliuda com sucesso.")
+    except Exception as e:
+        print (f'Ocorreu um erro {e}')        
+criaTabela('Teste2')
