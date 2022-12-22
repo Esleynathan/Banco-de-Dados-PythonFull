3503cf59012b7e5eef21551c891143f46f3828dc
Esley <esleynathan@hotmail.com>
2022-12-22 01:06:53 -0300

    CRUD PYTHON + MYSQL | Insert into + Select + Update + Delete

--- inicio.py
+++ inicio.py
@@ -13,16 +13,51 @@ con = pymysql.connect(
 def criaTabela(nomeTabela):
     try:   
         with con.cursor() as cursor:
-            cursor.execute("create table teste (nome varchar(50))")
+            cursor.execute(f"create table {nomeTabela} (nome varchar(50))")
         print("Tabela criada com sucesso.")
     except Exception as e:
         print (f'Ocorreu um erro {e}')
 
-def removerTabela(nomeTabela):
+def removeTabela(nomeTabela):
     try:   
         with con.cursor() as cursor:
             cursor.execute(f"drop table {nomeTabela}")
         print("Tabela excliuda com sucesso.")
     except Exception as e:
-        print (f'Ocorreu um erro {e}')        
-criaTabela('Teste2')
+        print (f'Ocorreu um erro {e}') 
+
+def insereTabela(nome):
+    try:   
+        with con.cursor() as cursor:
+            cursor.execute(f"INSERT INTO teste values ('{nome}')")
+        print("Valor inserido com sucesso.")
+    except Exception as e:
+        print (f'Ocorreu um erro {e}')
+
+def selectTabela(nomeTabela):
+    try:   
+        with con.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM ('{nomeTabela}')")
+            resultado = cursor.fetchall()
+            print (resultado)
+    except Exception as e:
+        print (f'Ocorreu um erro {e}')
+
+def updateTabela(nomeAlterar, nomeAlterado):
+    try:   
+        with con.cursor() as cursor:
+            cursor.execute(f"UPDATE teste SET nome = {nomeAlterar} WHERE nome = {nomeAlterado}")
+            print('Atualização realizada com sucesso.')
+    except Exception as e:
+        print (f'Ocorreu um erro {e}')
+
+def deleteTabela():
+    try:   
+        with con.cursor() as cursor:
+            cursor.execute(f"DELETE FROM  teste WHERE nome = 'marco'")
+            print('Remoção realizada com sucesso.')
+    except Exception as e:
+        print (f'Ocorreu um erro {e}')
+
+con.commit()
+con.close()
