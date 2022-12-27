d981f59739841725fffd5149ea88a30ad9430917
Esley <esleynathan@hotmail.com>
2022-12-26 23:16:41 -0300

    ORM | SQLalchemy - Foreign Key

--- ORM.py
+++ ORM.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -22,4 +22,17 @@ class Pessoa(Base):
     usuario = Column(String(20))
     senha = Column(String(10))
 
+class Categoria(Base):
+    __tablename__ = "categoria"
+    id = Column(Integer, primary_key = True)
+    categoria = Column(String(50))
+
+class Produto(Base):
+    __tablename__ = "produto"    
+    id = Column(Integer, primary_key = True)
+    produto = Column(String(50))
+    
+    idCategoria = Column(Integer, ForeignKey('categoria.id'))
+
+    
 Base.metadata.create_all(engine)
